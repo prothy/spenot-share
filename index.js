6fb75a2d37fa4bd20f85f57462583b29cd9f37f7
prothy <lukacsurgay@gmail.com>
2021-06-03 16:30:19 +0200

    init: install mongoose

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import routes from './controllers/routes'
 
 const app = express()
 const port = 3000
 
+// MONGOOSE INIT
+mongoose.connect('mongodb://localhost/spenot', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+    console.log('Connection to MongoDB succeeded.')
+})
+
+// EXPRESS INIT
 app.set('view engine', 'pug')
+app.use(express.static('static'))
 
 app.use(routes)
 
