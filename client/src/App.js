78d001490072204824156e3f6209ade2e3251fff
prothy <lukacsurgay@gmail.com>
2021-10-03 21:28:55 +0200

    refactor: remove api test

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+import './App.css'
 
 import Login from './components/Login.jsx'
 import User from './components/User.jsx'
 
 function App() {
-  const [test, setTest] = useState({})
 
-  useEffect(() => {
-    fetch('/api/react-test')
-    .then(res => res.json())
-    .then(data => setTest(data))
-  }, [])
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/user/0">User</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-  return (
-      <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/user/0">User</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/user/:id">
-            <User />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+                <Switch>
+                    <Route path="/user/:id">
+                        <User />
+                    </Route>
+                    <Route path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
