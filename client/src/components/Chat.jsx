2ca9c502d45885e795b3562661459b110adc82d4
prothy <lukacsurgay@gmail.com>
2021-10-03 21:30:00 +0200

    feat: add SongInput to Chat component

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import Message from './Message.jsx'
+import SongInput from './SongInput.jsx'
 
 const Chat = () => {
     const [messages, setMessages] = useState([])
@@ -10,13 +11,14 @@ const Chat = () => {
         ]
 
         setMessages(messageList)
-    }, [messages]);
+    }, [messages])
 
     return (
         <article>
             <section className="msg-list">
                 {messages.map(e => <Message content={e} />)}
             </section>
+            <SongInput />
         </article>
     )
 }
