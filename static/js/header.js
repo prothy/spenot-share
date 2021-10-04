d5816e736e11ec5a105de1d92797cef94f9f5787
prothy <lukacsurgay@gmail.com>
2021-10-04 21:04:11 +0200

    refact: change some file and folder names

--- static/js/header.js
+++
@@ -1,9 +0,0 @@
-if (username) {
-    document.querySelector('.header-links').innerHTML = `<span>Welcome, <a href="/user/${username}">${username}</a>.</span> <a href='/logout'>Logout</a>`
-}
-
-function getUsernameFromCookie() {
-    for (let element of document.cookie.split(';')) {
-        if (element.startsWith('name=')) return element.substr(5)
-    }
-}
