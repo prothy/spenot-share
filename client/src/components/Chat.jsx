667b0619bb47ec4648ac96c0cd79f0f5a185a2fd
prothy <lukacsurgay@gmail.com>
2021-10-07 20:34:19 +0200

    fix: add useCallback for getCurrentUser

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,13 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useHistory, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import Message from './Message.jsx'
 import SongInput from './SongInput.jsx'
 
-const Chat = () => {
-    const [messages, setMessages] = useState([])
-    const [user, setUser] = useState()
+import './css/Chat.css'
 
-    const getCurrentUser = async () => {
+const Chat = () => {
+    const history = useHistory()
+
+    const [messages, setMessages] = useState([])
+    const [currentUser, setCurrentUser] = useState()
+
+    const getCurrentUser = useCallback(async () => {
         const userObj = await fetch(process.env.REACT_APP_SERVER_URL + '/api/get-current-user', {
             credentials: 'include',
             mode: 'cors',
@@ -15,22 +19,22 @@ const Chat = () => {
             .then(res => res.json())
             .catch(e => console.error(e))
 
-        setUser(userObj.username)
-    }
+        userObj.username ? setCurrentUser(userObj.username) : history.push('/')
+    }, [history])
 
     useEffect(() => {
-        getCurrentUser()
+        if (!currentUser) getCurrentUser()
 
         const messageList = [
             'das', 'asdf', 'asdf'
         ]
 
         setMessages(messageList)
-    }, [])
+    }, [currentUser, getCurrentUser])
 
     return (
         <article>
-            <span>Currently logged in: <Link to={{pathname: '/user/' + user}}>{user}</Link></span>
+            <span>Currently logged in: <Link to={{pathname: '/user/' + currentUser}}>{currentUser}</Link></span>
             <section className="msg-list">
                 {messages.map(e => <Message content={e} />)}
             </section>
