e353d9a453e2c84bb9fba0c91d417e2bec63af78
prothy <lukacsurgay@gmail.com>
2021-10-12 15:26:01 +0200

    refact: move username fetch up one layer to User

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,41 +1,22 @@
-import React, {useState, useEffect, useCallback } from 'react'
-import { useHistory } from 'react-router'
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect } from 'react'
 import Message from './Message.jsx'
 import SongInput from './SongInput.jsx'
 
 import './css/Chat.scss'
 
 const Chat = () => {
-    const history = useHistory()
-
     const [messages, setMessages] = useState([])
-    const [currentUser, setCurrentUser] = useState()
-
-    const getCurrentUser = useCallback(async () => {
-        const userObj = await fetch(process.env.REACT_APP_SERVER_URL + '/api/get-current-user', {
-            credentials: 'include',
-            mode: 'cors',
-        })
-            .then(res => res.json())
-            .catch(e => console.error(e))
-
-        userObj.username ? setCurrentUser(userObj.username) : history.push('/')
-    }, [history])
 
     useEffect(() => {
-        if (!currentUser) getCurrentUser()
-
         const messageList = [
             'das', 'asdf', 'asdf'
         ]
 
         setMessages(messageList)
-    }, [currentUser, getCurrentUser])
+    }, [])
 
     return (
         <article>
-            <span>Currently logged in: <Link to={{pathname: '/user/' + currentUser}}>{currentUser}</Link></span>
             <section className="msg-list">
                 {messages.map(e => <Message content={e} />)}
             </section>
--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
 
+import Header from './Header.jsx'
 import ChatHeader from './ChatHeader.jsx'
 import Sidebar from './ChatHeader.jsx'
 import Chat from './Chat'
@@ -7,9 +9,32 @@ import Chat from './Chat'
 import './css/User.scss'
 
 const User = () => {
+    const history = useHistory()
+
+    const [currentUser, setCurrentUser] = useState()
+
+    const getCurrentUser = useCallback(async () => {
+        const userObj = await fetch(process.env.REACT_APP_SERVER_URL + '/api/get-current-user', {
+            credentials: 'include',
+            mode: 'cors',
+        })
+            .then(res => res.json())
+            .catch(e => console.error(e))
+
+        userObj.username ? setCurrentUser(userObj.username) : history.push('/')
+    }, [history])
+
+
+
+    useEffect(() => {
+        getCurrentUser()
+    }, [currentUser, getCurrentUser])
+
     return (
         <>
-            <ChatHeader />
+            <Header>
+                <ChatHeader user={currentUser} />
+            </Header>
             <main>
                 <Sidebar />
                 <Chat />
