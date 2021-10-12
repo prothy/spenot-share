500ec182aac36c1cb5cc5a99b705a37ca0f17b8a
prothy <lukacsurgay@gmail.com>
2021-10-12 16:11:03 +0200

    feat: add header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Header = ({children}) => {
     return (
         <header>
+            <h1 className="hdr-title">share app thang</h1>
             {children}
         </header>
     )
