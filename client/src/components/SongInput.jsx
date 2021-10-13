a2f1a10b9b6eef89ce758f3bb34fc71a5fc183dd
prothy <lukacsurgay@gmail.com>
2021-10-13 17:14:27 +0200

    feat: add frontend and backend SongInput handler fetch methods

--- client/src/components/SongInput.jsx
+++ client/src/components/SongInput.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const SongInput = ({val}) => {
-    const sendVal = (event) => {
+    const trimUrl = (url) => {
+        const splitUrl =  url.split(/(track\/|\?)+/g)
+        return splitUrl[2]
+    }
+
+    const sendVal = async (event) => {
+        const trimmedUrl = trimUrl(event.target.value)
         if (event.key === 'Enter')
         {
-            console.log(event.target.value)
+            console.log(trimmedUrl)
+            await fetch(process.env.REACT_APP_SERVER_URL + '/api/song/' + trimmedUrl, {
+                credentials: 'include',
+                mode: 'cors'
+            }).catch(e => console.error(e))
         }
     }
 
@@ -13,7 +23,7 @@ const SongInput = ({val}) => {
             placeholder="Enter Spotify URL"
             value={val.inputValue} 
             onChange={e => val.setInputValue(e.target.value)} 
-            onKeyDown={e => sendVal(e)}
+            onKeyDown={sendVal}
         />
     )
 }
