d8e90656569afc674168907d83023ca2a494464c
prothy <lukacsurgay@gmail.com>
2021-11-02 16:31:18 +0100

    refact: remove then/catch

--- client/src/components/chat/User.jsx
+++ client/src/components/chat/User.jsx
@@ -19,25 +19,31 @@ const User = () => {
     const [userOnPage, setUserOnPage] = useState({})
 
     const getCurrentUser = useCallback(async () => {
-        const userObj = await fetch(process.env.REACT_APP_SERVER_URL + '/api/get-current-user', {
-            credentials: 'include',
-            mode: 'cors',
-        })
-            .then(res => res.json())
-            .catch(e => console.error(e))
+        try {
+            const res = await fetch(process.env.REACT_APP_SERVER_URL + '/api/get-current-user', {
+                credentials: 'include',
+                mode: 'cors',
+            })
+            const userObj = await res.json()
 
-        userObj.username ? setCurrentUser(userObj.username) : history.push('/')
+            userObj.username ? setCurrentUser(userObj.username) : history.push('/')
+        } catch (e) {
+            console.error(e)
+        }
     }, [history])
 
     const getUserOnPage = useCallback(async () => {
-        const userObj = await fetch(process.env.REACT_APP_SERVER_URL + '/api/user/' + id, {
-            credentials: 'include',
-            mode: 'cors'
-        })
-            .then(res => res.json())
-            .catch(e => console.error(e))
+        try {
+            const res = await fetch(process.env.REACT_APP_SERVER_URL + '/api/user/' + id, {
+                credentials: 'include',
+                mode: 'cors'
+            })
+            const userObj = await res.json()
 
-        setUserOnPage(userObj)
+            setUserOnPage(userObj)
+        } catch (e) {
+            console.error(e)
+        }
     }, [id])
 
     useEffect(() => {
@@ -47,7 +53,6 @@ const User = () => {
 
     return (
         <>
-            {console.log(userOnPage)}
             <Header>
                 <ChatHeader user={currentUser} />
             </Header>
